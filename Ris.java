03882bc3bc4af825b346c3845dab37d7ca722e5d
mylzsd <liu863@purdue.edu>
2016-10-04 01:56:29 -0400

    Today is the beginning of my new life.

---
+++ Ris.java
@@ -0,0 +1,6 @@
+
+public class Ris {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Table.java
@@ -0,0 +1,9 @@
+
+public class Table {
+    int col_count, row_count;
+    boolean[] col_type; //ture - string, false - double
+    public Table(int col_count) {
+        this.col_count = col_count;
+        col_type = new boolean[col_count];
+    }
+}
