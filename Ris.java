d14531bc6a01082dfab8c2e994249fff719fd47f
mylzsd <liu863@purdue.edu>
2017-05-07 10:49:47 +0800

    no message

--- Ris.java
+++
@@ -1,8 +0,0 @@
-
-public class Ris {
-    public static void main(String[] args) {
-        Table tb = Reader.buildTable("training_file.txt");
-        RandomForest rf = new RandomForest(tb);
-        Reader.examination("testing_file.txt", rf);
-    }
-}
